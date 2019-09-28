946b0d16ac0b7950a3c0971ad7063540d3beb00a
Leonardo Ignacio Martins <leonardo_ignacio2010@hotmail.com>
2019-09-28 00:53:51 -0300

    commit inicial

---
+++ ruby/operadores.rb
@@ -0,0 +1,32 @@
+
+
+#numero1 = 0
+#numero2 = 0
+#total = 0
+
+#puts 'Informe o número 1:'  #Mensagem na tela
+#numero1 = gets.chomp.to_i   #Pega o valor via input do usuário e converte para integer
+
+#puts 'Informe o numero 2:'
+#numero2 = gets.chomp.to_i
+
+#total = numero1 + numero2
+#total = numero1 - numero2
+#total = numero1 * numero2
+#total = numero1 / numero2
+#puts total
+
+#Operadores de Comparação
+
+v1 = 10
+v2 = 10
+
+# maior que >
+# menor que <
+# maior ou igual >=
+# menor ou igual <=
+# diferente !=
+# igual a ==
+# igual eql?
+
+puts v1.eql?(v2)
