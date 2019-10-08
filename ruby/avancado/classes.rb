91f145d8a2bc93b476e2b433386d6e99294fcd47
Leonardo Martins <leonardo.martins@castgroup.com.br>
2019-10-08 14:41:56 -0300

    update aula OO

--- ruby/avancado/classes.rb
+++ ruby/avancado/classes.rb
@@ -5,10 +5,13 @@ class Conta
 
   def deposita(valor)
     self.saldo += valor #self é o this do Java
-    puts "Depositando a quantia de #{valor} reais." #interpolação de String... não converte a variável 'valor' para String
+    puts "Depositando a quantia de #{valor} reais."   #interpolação de String... não converte a variável 'valor' para String
   end
 end
 
 c = Conta.new
 
+c.saldo = 0.0   #Inicializar a variável para não dar erro de tipagem
+
 c.deposita(100.00)
+puts c.saldo
