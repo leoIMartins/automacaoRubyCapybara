84e0863d7943517a5d548d2ae13c676f36974073
Leonardo Martins <leonardo.martins@castgroup.com.br>
2019-10-09 13:39:54 -0300

    Aula exceptions

--- ruby/avancado/classes.rb
+++ ruby/avancado/classes.rb
@@ -1,17 +1,30 @@
 
 
 class Conta
-  attr_accessor :saldo
+  #atributos
+  attr_accessor :saldo, :nome
+
+  #construtor executado toda vez quando é identificado o comando 'new'
+  def initialize(nome)
+    #Inicializar a variável para não dar erro de tipagem... ficaria nulo e daria problema ao somar com float
+    self.saldo = 0.0
+    self.nome = nome
+  end
 
   def deposita(valor)
-    self.saldo += valor #self é o this do Java
-    puts "Depositando a quantia de #{valor} reais."   #interpolação de String... não converte a variável 'valor' para String
+    #self é o this do Java
+    self.saldo += valor
+    #interpolação de String... não converte a variável 'valor' para String
+    puts "Depositando a quantia de #{valor} reais na conta de #{self.nome}."
   end
 end
 
-c = Conta.new
-
-c.saldo = 0.0   #Inicializar a variável para não dar erro de tipagem
+#Nova instância da classe 'Conta'
+c = Conta.new('Léo')
 
 c.deposita(100.00)
 puts c.saldo
+
+c.deposita(10.00)
+puts c.saldo
+puts c.nome
---
+++ ruby/avancado/excecoes.rb
@@ -0,0 +1,22 @@
+
+#Estrutura do 'try catch' do Ruby
+# begin
+#     # Devo tentar alguma coisa
+#     file = File.open('./ola')
+#     if file
+#         puts file.read
+#     end
+# rescue Exception => e
+#     # Obter um possível erro
+#     puts e.message
+#     puts e.backtrace
+# end
+
+#Método com try catch
+def soma(n1, n2)
+    n1 + n2
+rescue Exception => e
+    puts 'Erro ao executar a soma'
+end
+
+soma('10', 5)
