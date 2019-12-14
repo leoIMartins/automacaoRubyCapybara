63aaef281106a35f454a5397a2dc9a10b6c3bfc0
Leonardo Ignacio Martins <leonardo_ignacio2010@hotmail.com>
2019-12-14 12:41:48 -0300

    Update aula Heranca e a Conta Poupanca

---
+++ ruby/unitarios/app/bank.rb
@@ -0,0 +1,29 @@
+class Conta
+  attr_accessor :saldo, :mensagem
+
+  def initialize(saldo)
+    self.saldo = saldo
+  end
+
+  def saca(valor, max, taxa)
+    if (valor > self.saldo)
+      self.mensagem = "Saldo insuficiente para saque :("
+    elsif (valor > max)
+      self.mensagem = "Limite máximo por saque é de R$ " + max.to_s
+    else
+      self.saldo -= valor + taxa
+    end
+  end
+end
+
+class ContaCorrente < Conta #Herança
+  def saca(valor, max = 700, taxa = 5.00)
+    super   #procura o método com mesmo nome na classe pai
+  end
+end
+
+class ContaPoupanca < Conta #Herança
+  def saca(valor, max = 500, taxa = 2.00)
+    super
+  end
+end
