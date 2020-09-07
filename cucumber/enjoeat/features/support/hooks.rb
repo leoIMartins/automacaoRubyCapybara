54d0dd0bf02105ba3f766612f06ea252b83d9f66
Martins <l.martins@dxc.com>
2020-09-07 15:42:18 -0300

    page objects everywhere

--- cucumber/enjoeat/features/support/hooks.rb
+++ cucumber/enjoeat/features/support/hooks.rb
@@ -1,7 +1,10 @@
 Before do
-    page.current_window.resize_to(1440, 900)
+  page.current_window.resize_to(1440, 900)
+
+  @rest_list_page = RestaurantListPage.new
+  @rest_page = RestaurantPage.new
 end
 
-Before('@bread_bakery') do
-    visit '/restaurants/bread-bakery/menu'
+Before("@bread_bakery") do
+  visit "/restaurants/bread-bakery/menu"
 end
