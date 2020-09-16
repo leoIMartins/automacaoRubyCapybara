f75c36bc4d17117c0a2df4af74c3e251507ac0cd
Martins <l.martins@dxc.com>
2020-09-16 16:05:29 -0300

    aula final de modulo

--- cucumber/enjoeat/features/support/env.rb
+++ cucumber/enjoeat/features/support/env.rb
@@ -1,9 +1,21 @@
-require 'capybara'
-require 'capybara/cucumber'
-require 'selenium-webdriver'
+require "capybara"
+require "capybara/cucumber"
+require "selenium-webdriver"
+
+@browser = ENV["BROWSER"]
 
 Capybara.configure do |config|
-    config.default_driver = :selenium_chrome
-    config.app_host = 'http://localhost:3000/'
-    config.default_max_wait_time = 10
+  case @browser
+  when "chrome"
+    @driver = :selenium_chrome
+  when "firefox"
+    @driver = :selenium
+  when "chrome_headless"
+    @driver = :selenium_chrome_headless
+  when "firefox_headless"
+    @driver = :selenium_headless
+  end
+  config.default_driver = @driver
+  config.app_host = "http://localhost:3000/"
+  config.default_max_wait_time = 10
 end
--- cucumber/enjoeat/features/support/hooks.rb
+++ cucumber/enjoeat/features/support/hooks.rb
@@ -16,9 +16,11 @@ Before("@green_food") do
   visit "/restaurants/green-food/menu"
 end
 
-After do
-  shot_file = page.save_screenshot("log/screenshot.png")        # Cucumber tira screenshot temporário
-  shot_b64 = Base64.encode64(File.open(shot_file, "rb").read)   # Conversão para base 64
-  # embed(shot_b64, "image/png", "Screenshot")   # Cucumber anexa o screenshot no report
-  attach(shot_b64, "png")
+After do |scenario|
+  if scenario.failed?
+    shot_file = page.save_screenshot("log/screenshot.png")        # Cucumber tira screenshot temporário
+    shot_b64 = Base64.encode64(File.open(shot_file, "rb").read)   # Conversão para base 64
+    # embed(shot_b64, "image/png", "Screenshot")   # Cucumber anexa o screenshot no report
+    attach(shot_b64, "png")
+  end
 end
