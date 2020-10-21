45fcd243bc67238955a8d0168f01a65db51b5af6
Leonardo Martins <leonardo_ignacio2010@hotmail.com>
2020-10-21 00:25:21 -0300

    Aula novo modulo Jenkins

--- nflix/features/support/env.rb
+++ nflix/features/support/env.rb
@@ -9,8 +9,19 @@ World(Helpers)  # Todos os recursos do módulo Helpers são nativos dentro da ex
 
 CONFIG = YAML.load_file(File.join(Dir.pwd, "features/support/config/#{ENV["ENV_TYPE"]}.yaml"))
 
+case ENV["BROWSER"]
+when "firefox"
+  @driver = :selenium
+when "chrome"
+  @driver = :selenium_chrome
+when "headless"
+  @driver = :selenium_chrome_headless
+else
+  puts "Invalid Browser"
+end
+
 Capybara.configure do |config|
-  config.default_driver = :selenium
+  config.default_driver = @driver
   config.app_host = CONFIG["url"]
   config.default_max_wait_time = 5
 end
--- nflix/features/support/hooks.rb
+++ nflix/features/support/hooks.rb
@@ -12,3 +12,11 @@ Before("@login") do
   @login_page.go
   @login_page.with(user["email"], user["pass"])
 end
+
+After do |scenario|
+  #if scenario.failed?
+  temp_shot = page.save_screenshot("log/temp_shot.png")
+  screenshot = Base64.encode64(File.open(temp_shot).read)
+  embed(screenshot, "image/png", "Screenshot")
+  #end
+end
